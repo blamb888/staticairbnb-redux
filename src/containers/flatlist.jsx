852cbb7f7522be13beed5b0d083d34742eef20cd
Brandon Lamb <blamb888@gmail.com>
2021-05-06 20:55:21 +0900

    create app component and flatlist, map containers

---
+++ src/containers/flatlist.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Flatlist extends Component {
+  render() {
+    <div>
+      TODO
+    </div>
+  }
+}
+
+export default Flatlist;
---
+++ src/containers/map.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  render() {
+    <div>
+      TODO
+    </div>
+  }
+}
+
+export default Map;
