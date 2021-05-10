fbaa335326795653c9325668fb387e9a0636c0c7
Brandon Lamb <blamb888@gmail.com>
2021-05-10 20:01:15 +0900

    Fix style

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,18 +3,18 @@ import React, { Component } from 'react';
 class Flat extends Component {
   render() {
     return (
-      <div class="flat card-trip">
-        <img src={this.props.flat.imageUrl} alt="" />
-        <div class="card-trip-infos">
+      <div className="flat card-trip">
+        <img id="cardImage" src={this.props.flat.imageUrl} alt="" />
+        <div className="card-trip-infos">
           <div>
             <h2>{this.props.flat.name}</h2>
             <p>Short description here!</p>
           </div>
-          <h2 class="card-trip-pricing">
+          <h2 className="card-trip-pricing">
             {this.props.flat.price}
             &nbsp;{this.props.flat.priceCurrency}
           </h2>
-          <img src="https://kitt.lewagon.com/placeholder/users/krokrob" class="card-trip-user avatar-bordered" />
+          <img src="https://kitt.lewagon.com/placeholder/users/krokrob" className="card-trip-user avatar-bordered" alt="" />
         </div>
       </div>
 
