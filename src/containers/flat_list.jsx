4dfd940a211eec98e2c0bf65f579d988975dbcb8
Brandon Lamb <blamb888@gmail.com>
2021-05-11 18:58:21 +0900

    update package.json with latest version of react/react dom

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 import Flat from '../components/flat';
 import setFlats from '../actions';
 // eslint-disable-next-line react/prefer-stateless-function
-class Flatlist extends Component {
-
+class FlatList extends Component {
   // TEMPORARY CODE TO INTEGRATE HTML
-  // static defaultProps = {
-    //   flats: [{
-  //     name: "Charm at the Steps of Montmartre",
-  //     imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-  //     price: 164,
-  //     priceCurrency: "EUR"
-  //   }]
-  // }
+  static defaultProps = {
+    flats: [{
+      name: "Charm at the Steps of Montmartre",
+      imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+      price: 164,
+      priceCurrency: "EUR"
+    }]
+  }
 
   componentWillMount() {
     this.props.setFlats();
@@ -43,4 +42,4 @@ function mapStateToProps(state) {
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps) (Flatlist);
+export default connect(mapStateToProps, mapDispatchToProps)(FlatList);
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
-import App from './components/app';
 
+import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 import flatsReducer from './reducers/flats_reducer';
